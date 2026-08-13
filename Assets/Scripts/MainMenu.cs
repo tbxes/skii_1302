30f2f82c58d4abe658265dcaec1a2fea8c285284
tbxes <every2549@gmail.com>
2026-08-14 03:43:00 +0700

    kirei

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainM : MonoBehaviour
 {
     public void Startgame()
     {
         SceneManager.LoadScene("Scene01");
-
     }
 
     public void Exit()
